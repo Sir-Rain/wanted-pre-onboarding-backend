df224d4d6f465b52b0c5d21b92969d419fed48b1
Sir-Rain <liciaan@kakao.com>
2023-07-30 19:42:10 +0900

    add: express init code to app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import http from 'http';
+
+
+const PORT = 8080;
+
+const app = express();
+
+
+
+const server = http.createServer(app);
+server.listen(PORT);
+server.on('listening', ()=> {
+    console.log('Server is Running ON: ' + PORT);
+})
