47b985b058517218508cd63e45efa6ec7c875061
Sir-Rain <liciaan@kakao.com>
2023-07-30 20:02:07 +0900

    formatting

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
-import express from 'express'
+import express from 'express';
 import http from 'http';
-
 
 const PORT = 8080;
 
 const app = express();
 
-
-
 const server = http.createServer(app);
 server.listen(PORT);
-server.on('listening', ()=> {
-    console.log('Server is Running ON: ' + PORT);
-})
+server.on('listening', () => {
+  console.log('Server is Running ON: ' + PORT);
+});
