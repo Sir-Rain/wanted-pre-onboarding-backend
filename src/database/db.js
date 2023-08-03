490187a4c607ff590dbf4ca918e047554d40ffaf
Sir-Rain <liciaan@kakao.com>
2023-08-03 22:39:28 +0900

    add: db settings to db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,16 @@
 import { Sequelize } from 'sequelize';
+import config from 'config';
+
+const database = config.get('database');
+
+const { host, port, username, password, schema } = database;
 
 export const sequelize = new Sequelize({
+  host,
+  port,
+  username,
+  password,
+  database: schema,
   dialect: 'mysql',
 });
 
