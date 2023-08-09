c4136f4e8510b48614c9a2ec260290b6aec1e7e5
Sir-Rain <liciaan@kakao.com>
2023-08-09 11:03:53 +0900

    add: not fount end point

--- src/app.js
+++ src/app.js
@@ -8,3 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Not Found EndPoint' });
+});
